c2005be5d0c850153981313d0d48e4f0bbecaef0
Adnan Adiatman <adnanadiatman@gmail.com>
2018-06-03 22:50:55 +0700

    backup b

--- lap.php
+++ lap.php
@@ -207,15 +207,15 @@ if(empty($_SESSION['admin'])){
             //script untuk menampilkan data
             $query = mysqli_query($config,  "SELECT * FROM tbl_disposisisurat 
                                              LEFT JOIN tbl_tl ON tbl_tl.id_disposisi = tbl_disposisisurat.id_disposisi 
-                                             LEFT JOIN tbl_user ON tbl_tl.kepada = tbl_user.id_user 
+                                             LEFT JOIN tbl_user ON tbl_disposisisurat.kepada = tbl_user.id_user 
                                              Where `status`='$status' $search ORDER by tbl_disposisisurat.id_disposisi DESC LIMIT $curr, $limit");
             $query1 = mysqli_query($config, "SELECT * FROM tbl_disposisitlp 
                                              LEFT JOIN tbl_tl_tlp ON tbl_tl_tlp.id_disposisitlp = tbl_disposisitlp.id_disposisitlp 
-                                             LEFT JOIN tbl_user ON tbl_tl_tlp.kepada = tbl_user.id_user                                             
+                                             LEFT JOIN tbl_user ON tbl_disposisitlp.kepada = tbl_user.id_user                                             
                                              Where `status`='$status' $search ORDER by tbl_disposisitlp.id_disposisitlp DESC LIMIT $curr, $limit");
             $query2 = mysqli_query($config, "SELECT * FROM tbl_disposisilain 
                                              LEFT JOIN tbl_tl_lain ON tbl_tl_lain.id_disposisilain = tbl_disposisilain.id_disposisilain 
-                                             LEFT JOIN tbl_user ON tbl_tl_lain.kepada = tbl_user.id_user 
+                                             LEFT JOIN tbl_user ON tbl_disposisilain.kepada = tbl_user.id_user 
                                              Where `status`='$status' $search ORDER by tbl_disposisilain.id_disposisilain DESC LIMIT $curr, $limit");
             $no = 1;
             if(mysqli_num_rows($query) > 0 || mysqli_num_rows($query1) > 0 || mysqli_num_rows($query2) > 0){
